f6a51f6c620613a042c807c486c62ae592f8abec
Abdulaziz780 <akhusanov410@gmail.com>
2024-04-18 12:28:36 +0500

    izi

---
+++ main.js
@@ -0,0 +1,7 @@
+let etmiman1 = prompt("ismingizni kiriting")
+let etmiman2 = prompt("space dagi id ni kiriting")
+let etmiman3 = prompt("spase da gi parol ni kiriting")
+
+let obj = {ismingiz: etmiman1, id: etmiman2, parol: etmiman3,} 
+
+console.log(obj);
